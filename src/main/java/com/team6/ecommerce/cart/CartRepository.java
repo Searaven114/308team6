0828f08783f845251cd051a199320b272255e1ac
zssudek <zeynepkurnaz@sabanciuniv.edu>
2024-11-05 18:48:25 +0300

    cart entity and API Endpoints

---
+++ src/main/java/com/team6/ecommerce/cart/CartRepository.java
@@ -0,0 +1,9 @@
+package com.team6.ecommerce.cart;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends MongoRepository<Cart, String> {
+    Cart findByUserId(String userId);
+}
