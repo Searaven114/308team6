72cc20f562e190dc1aa1b453202191cfc4ebf4ea
aserdardem <serdar.demircan@sabanciuniv.edu>
2024-11-11 23:09:20 +0300

    Update CommentRepository.java
    fixed import statement

--- src/main/java/com/team6/ecommerce/comment/CommentRepository.java
+++ src/main/java/com/team6/ecommerce/comment/CommentRepository.java
@@ -1,4 +1,4 @@
-package com.borau.cs308demo.comment;
+package com.team6.ecommerce.comment;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
