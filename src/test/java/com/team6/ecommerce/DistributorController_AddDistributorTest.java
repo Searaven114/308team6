7a574d46f6e825b88ae8643b0461b5c851cf6050
zssudek <zeynepkurnaz@sabanciuniv.edu>
2024-12-02 13:27:35 +0300

    Add DistributorController_AddDistributorTest to verify distributor creation via API

---
+++ src/test/java/com/team6/ecommerce/DistributorController_AddDistributorTest.java
@@ -0,0 +1,43 @@
+import com.team6.ecommerce.distributor.Distributor;
+import com.team6.ecommerce.distributor.DistributorController;
+import com.team6.ecommerce.distributor.DistributorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(DistributorController.class)
+public class DistributorController_AddDistributorTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DistributorService distributorService;
+
+    @Test
+    public void testAddDistributor_Success() throws Exception {
+        // Arrange
+        Distributor distributor = new Distributor();
+        distributor.setId("1");
+        distributor.setName("New Distributor");
+
+        when(distributorService.addDistributor(any(Distributor.class))).thenReturn(distributor);
+
+        // Act & Assert
+        mockMvc.perform(post("/api/distributors")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"New Distributor\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("New Distributor"));
+
+        verify(distributorService, times(1)).addDistributor(any(Distributor.class));
+    }
+}
