83d2d6f58cf3935eee7900495da4e61e7b90f9c5
ranaerkan <ranaerkan@sabanciuniv.edu>
2025-01-12 19:16:19 +0000

    add RatingControllerTest

---
+++ src/test/java/com/team6/ecommerce/RatingControllerTest.java
@@ -0,0 +1,44 @@
+package com.team6.ecommerce;
+
+import com.team6.ecommerce.rating.RatingController;
+import com.team6.ecommerce.rating.RatingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RatingControllerTest {
+
+    @InjectMocks
+    private RatingController ratingController;
+
+    @Mock
+    private RatingService ratingService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+    /*
+    @Test
+    void testAddRating() {
+        String productId = "product123";
+        int ratingValue = 5;
+
+        when(ratingService.addRating(anyString(), eq(productId), eq(ratingValue)))
+                .thenReturn("Rating added successfully.");
+
+        ResponseEntity<String> response = ratingController.addRating(productId, ratingValue);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("Rating added successfully.", response.getBody());
+    }
+    */
+}
+
